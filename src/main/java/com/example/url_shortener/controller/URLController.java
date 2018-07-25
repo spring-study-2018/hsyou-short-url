f985076c83cf8f9658f6347d83d26a70e04f32ff
hsu <realhs01@gmail.com>
2018-07-25 21:33:01 +0900

    Append Tester

--- src/main/java/com/example/url_shortener/controller/URLController.java
+++ src/main/java/com/example/url_shortener/controller/URLController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api")
 public class URLController {
 
     @Autowired
